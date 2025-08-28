bd1bef71c14450abab66248d8db627c6d25fc5a7
jaslynmiura <jaslyntatsuko@gmail.com>
2025-08-28 15:53:30 -0700

    created a graph cheehooo

--- scratch/try2_fig3_remake.R
+++ scratch/try2_fig3_remake.R
@@ -9,6 +9,7 @@ library(tidyverse)
 library(here)
 library(lubridate)
 library(janitor)
+library(patchwork)
 
 # read in the files ------------------------------------------------------------
 # reading in each file and cleaning the variable names so it's easier handle
@@ -77,37 +78,103 @@ source("moving_avg_func.R")
 #                  conc = concentration, 
 #                  win_size_wks = 9)
 
-  
-# trying to add this to the entire data set
-stream_data %>% 
+# applying our function to the entire PRM data set
+# stream_data$calc_moving_avg <- sapply(
+#   stream_data$sample_date,
+#   moving_average,
+#   dates = stream_data$sample_date,
+#   conc = stream_data$concentration,
+#   win_size_wks = 9
+# )
+
+# full analysis combined data sets ---------------------------------------------
+
+# calculates the moving average for all streams and all nutrients
+stream_moving_avg <- stream_data %>% 
   group_by(sample_id, nutrient) %>% 
   mutate(sample_id = as.factor(sample_id),
          nutrient = as.factor(nutrient)) %>% 
   mutate(moving_avg = sapply(
     sample_date,
     moving_average,
-    dates = stream_data$sample_date,
-    conc = stream_data$concentration,
+    dates = as.Date(sample_date),
+    conc = concentration,
     win_size_wks = 9
   ))
 
+# recreate figure 3 ------------------------------------------------------------
 
+stream_moving_avg <- stream_moving_avg %>% 
+  mutate(year = year(sample_date))
 
+potassium_moving_avg <- stream_moving_avg %>% 
+  filter(nutrient == "k") %>% 
+  filter(year >= 1989,
+         year <= 1998 )
+nitrate_moving_avg <- stream_moving_avg %>% 
+  filter(nutrient == "no3_n") %>% 
+  filter(year >= 1988,
+         year <= 1998 )
+magnesium_moving_avg <- stream_moving_avg %>% 
+  filter(nutrient == "mg") %>% 
+  filter(year >= 1988,
+         year <= 1998 )
+calcium_moving_avg <- stream_moving_avg %>% 
+  filter(nutrient == "ca") %>% 
+  filter(year >= 1988,
+         year <= 1998 )
+amm_nitrate_moving_avg <- stream_moving_avg %>% 
+  filter(nutrient == "nh4_n") %>% 
+  filter(year >= 1988,
+         year <= 1998 )
 
+# plotting potassium
+potassium_plot <- ggplot(data = potassium_moving_avg,
+       aes(x = sample_date, y = moving_avg, linetype = sample_id)) +
+  geom_line() +
+  # geom_vline(aes(xintercept = c(1998, 0)), linewidth = 1.5,
+  #            color = "black", linetype = "dashed") +
+  theme_minimal() + 
+  labs(x = NULL,
+       y = "K mg l^-1") + 
+  theme(legend.position = c(0.9, 0.9)) 
+potassium_plot
 
+# plotting nitrate
+nitrate_plot <- ggplot(data = nitrate_moving_avg,
+       aes(x = sample_date, y = moving_avg, linetype = sample_id)) +
+  geom_line() +
+  theme_minimal() + 
+  labs(x = NULL,
+       y = "NO3-N mg l^-1") +
+  theme(legend.position = "none")
+nitrate_plot
 
+# plotting magnesium
+magnesium_plot <- ggplot(data = magnesium_moving_avg,
+       aes(x = sample_date, y = moving_avg, linetype = sample_id)) +
+  geom_line() +
+  theme_minimal() +
+  labs(x = NULL,
+       y = "Mg l^-1") + 
+  theme(legend.position = "none")
 
+# plotting calcium
+calcium_plot <- ggplot(data = calcium_moving_avg,
+       aes(x = sample_date, y = moving_avg, linetype = sample_id)) +
+  geom_line() + 
+  theme_minimal() +
+  labs(x = NULL,
+       y = "Ca l^-1") + 
+  theme(legend.position = "none")
 
+# plotting ammonium nitrate
+amm_nitrate_plot <- ggplot(data = amm_nitrate_moving_avg,
+       aes(x = sample_date, y = moving_avg, linetype = sample_id)) +
+  geom_line() + 
+  theme_minimal() +
+  labs(x = "Years",
+       y = "NH4-N mg l^-1") + 
+  theme(legend.position = "none")
 
-# applying our function to the entire PRM data set
-stream_data$calc_moving_avg <- sapply(
-  stream_data$sample_date,
-  moving_average,
-  dates = stream_data$sample_date,
-  conc = stream_data$concentration,
-  win_size_wks = 9
-)
-
-# full analysis combined data sets ---------------------------------------------
-
-
+(potassium_plot / nitrate_plot / magnesium_plot / calcium_plot / amm_nitrate_plot)
