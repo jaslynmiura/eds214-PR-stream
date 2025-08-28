09c22590c0dbf131a136559bc78aa49d7e74364d
jaslynmiura <jaslyntatsuko@gmail.com>
2025-08-28 11:17:22 -0700

    project progress?

--- moving_avg_func.R
+++ moving_avg_func.R
@@ -5,7 +5,7 @@ moving_average <- function(focal_date, dates, conc, win_size_wks) {
   # Find the associated concentrations
   window_conc <- conc[is_in_window]
   # Calculate the mean
-  result <- mean(window_conc)
+  result <- mean(window_conc, na.rm = TRUE)
   
   return(result)
 }
--- scratch/try2_fig3_remake.R
+++ scratch/try2_fig3_remake.R
@@ -23,30 +23,91 @@ BQ3 <- read_csv(here("data", "QuebradaCuenca3-Bisley.csv")) %>%
 
 # cleaning data ----------------------------------------------------------------
 # selecting the columns of data that we are interested in
-# also want to try to pivot the dataframe so that all the concentrations are in one column but nutrient is defined by row
-PRM_clean <- PRM %>% 
-  select(sample_id, sample_date, k, no3_n, mg, ca, nh4_n) # %>% 
-  # pivot_longer(k:nh4_n,
-  #              names_to = ,
-  #              values_to = concentration)
+# also want to try to pivot the data frame so that all the concentrations are in one column but nutrient is defined by row
+PRM_clean_long <- PRM %>% 
+  pivot_longer(c("k", "no3_n", "mg", "ca", "nh4_n"),
+               names_to = "nutrient",
+               values_to = "concentration") %>% 
+  select(sample_id, sample_date, nutrient, concentration)
+
+BQ1_clean_long <- BQ1 %>% 
+  pivot_longer(c("k", "no3_n", "mg", "ca", "nh4_n"),
+               names_to = "nutrient",
+               values_to = "concentration") %>% 
+  select(sample_id, sample_date, nutrient, concentration)
+
+BQ2_clean_long <- BQ2 %>% 
+  pivot_longer(c("k", "no3_n", "mg", "ca", "nh4_n"),
+               names_to = "nutrient",
+               values_to = "concentration") %>% 
+  select(sample_id, sample_date, nutrient, concentration)
+
+BQ3_clean_long <- BQ3 %>% 
+  pivot_longer(c("k", "no3_n", "mg", "ca", "nh4_n"),
+               names_to = "nutrient",
+               values_to = "concentration") %>% 
+  select(sample_id, sample_date, nutrient, concentration)
+
+# joinging the data from the different stream sites into one data set
+stream_data <- PRM_clean_long %>% 
+  full_join(BQ1_clean_long) %>% 
+  full_join(BQ2_clean_long) %>% 
+  full_join(BQ3_clean_long)
 
 # running the analysis on just the PRM data set --------------------------------
 # testing that our moving_avg function works on one stream data set
 source("moving_avg_func.R")
 
-# testing that the function gives us a realistic output for ONE input
-ca_ma_once <- moving_average(focal_date = as.Date("1991-09-03"),
-                             dates = PRM_clean$sample_date,
-                             conc = PRM_clean$ca,
-                             win_size_wks = 9)
+# # testing that the function gives us a realistic output for ONE input
+# ca_ma_once <- moving_average(focal_date = as.Date("1991-09-03"),
+#                              dates = PRM_clean$sample_date,
+#                              conc = PRM_clean$ca, # since we are going to pivot longer we it would no longer be data$con
+#                              win_size_wks = 9)
+
+# PRM_clean_long %>% 
+#   group_by(sample_id, nutrient) %>% 
+#   mutate(calc_moving_avg = sapply(PRM_clean_long, moving_average(focal_date = sample_date,
+#                         dates = sample_date,
+#                         conc = concentration, # since we are going to pivot longer we it would no longer be data$con
+#                         win_size_wks = 9)))
+# 
+# 
+#   moving_average(focal_date = sample_date,
+#                  dates = sample_date,
+#                  conc = concentration, 
+#                  win_size_wks = 9)
+
+  
+# trying to add this to the entire data set
+stream_data %>% 
+  group_by(sample_id, nutrient) %>% 
+  mutate(sample_id = as.factor(sample_id),
+         nutrient = as.factor(nutrient)) %>% 
+  mutate(moving_avg = sapply(
+    sample_date,
+    moving_average,
+    dates = stream_data$sample_date,
+    conc = stream_data$concentration,
+    win_size_wks = 9
+  ))
+
+
+
+
+
+
+
+
 
 # applying our function to the entire PRM data set
-PRM_clean$calc_rolling_ca <- sapply(
-  PRM_clean$sample_date,
+stream_data$calc_moving_avg <- sapply(
+  stream_data$sample_date,
   moving_average,
-  dates = PRM_clean$sample_date,
-  conc = PRM_clean$ca,
+  dates = stream_data$sample_date,
+  conc = stream_data$concentration,
   win_size_wks = 9
 )
 
 # full analysis combined data sets ---------------------------------------------
+
+
