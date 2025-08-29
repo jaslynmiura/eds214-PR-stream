03b840e4e13766c38592138bff6940fe96ace79d
jaslynmiura <jaslyntatsuko@gmail.com>
2025-08-29 11:52:53 -0700

    progress

--- scratch/data_cleaning_and_tidying.R
+++ scratch/data_cleaning_and_tidying.R
@@ -15,6 +15,9 @@ BQ2 <- read_csv(here("data", "QuebradaCuenca2-Bisley.csv")) %>%
 BQ3 <- read_csv(here("data", "QuebradaCuenca3-Bisley.csv")) %>% 
   clean_names()
 
+# read in the moving_average function ------------------------------------------
+source(here("moving_avg_func.R"))
+
 # cleaning data ----------------------------------------------------------------
 # selecting the columns of data that we are interested in
 # also want to try to pivot the data frame so that all the concentrations are in one column but nutrient is defined by row
@@ -47,3 +50,45 @@ stream_data <- PRM_clean_long %>%
   full_join(BQ1_clean_long) %>% 
   full_join(BQ2_clean_long) %>% 
   full_join(BQ3_clean_long)
+
+# using the moving_average function to calculate the moving average of concentrations
+stream_moving_avg <- stream_data %>% 
+  group_by(sample_id, nutrient) %>% 
+  mutate(sample_id = as.factor(sample_id),
+         nutrient = as.factor(nutrient)) %>% 
+  mutate(moving_avg = sapply(
+    sample_date,
+    moving_average,
+    dates = as.Date(sample_date),
+    conc = concentration,
+    win_size_wks = 9
+  ))
+
+# creating a year column to so we can filter our data to our time frame of interest (1989-1998)
+stream_moving_avg <- stream_moving_avg %>% 
+  mutate(year = year(sample_date))
+
+# creating subsets of each nutrient concentrations to create seperate graphs
+potassium_moving_avg <- stream_moving_avg %>% 
+  filter(nutrient == "k") %>% 
+  filter(year >= 1988,
+         year <= 1998 )
+nitrate_moving_avg <- stream_moving_avg %>% 
+  filter(nutrient == "no3_n") %>% 
+  filter(year >= 1988,
+         year <= 1998 )
+magnesium_moving_avg <- stream_moving_avg %>% 
+  filter(nutrient == "mg") %>% 
+  filter(year >= 1988,
+         year <= 1998 )
+calcium_moving_avg <- stream_moving_avg %>% 
+  filter(nutrient == "ca") %>% 
+  filter(year >= 1988,
+         year <= 1998 )
+amm_nitrate_moving_avg <- stream_moving_avg %>% 
+  filter(nutrient == "nh4_n") %>% 
+  filter(year >= 1988,
+         year <= 1998 )
+
+# defining the date of Hurricane Hugo, so it can be highlighted on our graph
+hugo <- as.Date("1989-09-20")
